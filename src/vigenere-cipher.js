dd4dbb0ee7baf8b011bbecc4f1700e34bf603785
Maksim Diubo <diubomaksim@gmail.com>
2020-02-29 14:59:49 +0300

    feat: resolve basic-js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,5 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
 }
 
 module.exports = VigenereCipheringMachine;
